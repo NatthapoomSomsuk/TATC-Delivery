6223436e7bab8492702c00a2e30c455c1cb70667
Dragon Style Studio <natthapoom.somsuk@gmail.com>
2024-02-15 14:29:25 +0700

    last

--- api/empgetorder.php
+++ api/empgetorder.php
@@ -4,15 +4,16 @@ include('../controllers/con_db.php');
 if ($requestMethod == 'GET') {
     if(isset($_GET['gettime'])){
         $empid = $_GET['gettime'];
-        $sqlchack = "SELECT * 
+        $sqlchack = "SELECT *
         FROM q_emp
-        INNER JOIN `days` ON `q_emp`.`day_id` = `days`.`day_id`
+        INNER JOIN days ON q_emp.`day_id` = days.`day_id`
         INNER JOIN time ON time.time_id = q_emp.time_id
         INNER JOIN status_empq ON status_empq.status_empqid = q_emp.status_empqid 
         WHERE emp_id = '$empid'
         AND day_name = DAYNAME(NOW())
-        AND time_name > TIME(NOW())
-        AND time_name < DATE_ADD(TIME(NOW()), INTERVAL 1 HOUR)
+        AND time_name < DATE_ADD(time_name, INTERVAL 1 HOUR)
+        AND  TIME(NOW()) BETWEEN DATE_ADD(time_name, INTERVAL 0 HOUR) AND DATE_ADD(time_name, INTERVAL 1 HOUR);
+        
         AND status_name = 'ว่าง'";
         $sqlchack_q = mysqli_query($conn,$sqlchack);
         if(mysqli_num_rows($sqlchack_q)>0){
